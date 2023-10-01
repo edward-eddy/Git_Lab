d0db4851da07a562d2b4a4c2bc7b8a716156af1d
SaraWilliam789 <sarawillim1993@gmail.com>
2023-10-01 15:40:41 +0300

    the first componet of navbar

---
+++ src/app/sara-navbar/sara-navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sara-navbar',
+  templateUrl: './sara-navbar.component.html',
+  styleUrls: ['./sara-navbar.component.scss']
+})
+export class SaraNavbarComponent {
+
+}
