9271b360c5db004fa60dd0bcc2edcc18d8a78db2
Randa-10 <randa010011012@gmail.com>
2023-10-01 15:40:41 +0300

    cart componnent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EdwardComponent } from './edward/edward.component';
+import { CartComponent } from './Component/cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EdwardComponent
+    EdwardComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
